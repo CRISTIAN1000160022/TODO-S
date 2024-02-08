c60877ec092b55a7c166d0787a9afa90c4ebc07a
Cristian Andres Jimenez Arciniegas <cajimenez@landers.com.co>
2024-02-08 16:21:39 -0500

    Previous version configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 import App from "./pages/home";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render(<App />);
+ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
